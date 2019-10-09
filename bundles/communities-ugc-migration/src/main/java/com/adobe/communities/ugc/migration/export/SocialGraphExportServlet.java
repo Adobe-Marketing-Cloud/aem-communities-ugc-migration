1b6b8a701329e0428da2366354815c490b112301
abhigarg <abhigarg@adobe.com>
2019-10-09 12:46:29 +0530

    MSRP to ASRP graph migration

--- bundles/communities-ugc-migration/src/main/java/com/adobe/communities/ugc/migration/export/SocialGraphExportServlet.java
+++ bundles/communities-ugc-migration/src/main/java/com/adobe/communities/ugc/migration/export/SocialGraphExportServlet.java
@@ -11,13 +11,17 @@
  **************************************************************************/
 package com.adobe.communities.ugc.migration.export;
 
+import com.adobe.communities.ugc.migration.util.Constants;
 import com.adobe.cq.social.graph.SocialGraph;
 import com.adobe.cq.social.graph.Vertex;
 import com.adobe.granite.socialgraph.Direction;
 import com.adobe.granite.socialgraph.Relationship;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.felix.scr.annotations.*;
+import org.apache.jackrabbit.JcrConstants;
 import org.apache.jackrabbit.api.security.user.User;
+import org.apache.jackrabbit.oak.spi.security.user.UserConstants;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.resource.Resource;
@@ -31,7 +35,7 @@ import org.slf4j.LoggerFactory;
 
 import javax.jcr.RepositoryException;
 import javax.servlet.ServletException;
-import java.io.IOException;
+import java.io.*;
 
 @Component(label = "Social Graph Exporter",
         description = "Moves social graph schema into a zip archive for storage or re-import", specVersion = "1.1")
@@ -43,21 +47,58 @@ public class SocialGraphExportServlet extends SlingSafeMethodsServlet {
     @Reference
     protected SlingRepository repository;
     
-    Logger logger = LoggerFactory.getLogger(this.getClass());
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private static final String JCR_PRIMARY_TYPE = JcrConstants.JCR_PRIMARYTYPE;
+    private static final String AUTHORIZABLE_USER = UserConstants.NT_REP_AUTHORIZABLE_FOLDER ;
+    private static final String USER = UserConstants.NT_REP_USER ;
+
+    private Writer responseWriter;
 
     protected void doGet(final SlingHttpServletRequest request, final SlingHttpServletResponse response)
             throws ServletException, IOException {
 
-        final JSONWriter writer = new JSONWriter(response.getWriter());
-        writer.setTidy(true);
         final SocialGraph graph = request.getResourceResolver().adaptTo(SocialGraph.class);
         final String path = StringUtils.stripEnd(request.getRequestParameter("path").getString(), "/");
         final String relType =request.getRequestParameter("relType") !=null ? request.getRequestParameter("relType").toString():null;
         final String typeS =request.getRequestParameter("typeS") !=null ? request.getRequestParameter("typeS").toString():null;
+        final String fileName =request.getRequestParameter("fileName") !=null ? request.getRequestParameter("fileName").toString():"null";
 
-        if(relType == null || typeS ==null ){
-        	logger.error("Required parameters are not present. Exiting");
-        	throw new ServletException("Required parameters are not present. Exiting");
+        if(fileName == null){
+            logger.error("fileName parameter is not present. Exiting");
+            throw new ServletException("fileName parameter is not present. Exiting");
+        }
+        if(relType == null){
+        	logger.error("relType parameters us not present. Exiting");
+        	throw new ServletException("relType parameters us not present. Exiting");
+        }else if(typeS ==null){
+            logger.error("typeS parameters us not present. Exiting");
+            throw new ServletException("typeSO37\n" +
+                    "518Adobe-Marketing-Cloud/aem-communities-ugc-migration\n" +
+                    " Code Issues 3 Pull requests 2 Projects 0 Wiki Security Insights\n" +
+                    "CQ-4279355 export code for migration from MSRP to ASRP. #25\n" +
+                    " Open\tabhishekgarg18 wants to merge 2 commits into Adobe-Marketing-Cloud:master from abhishekgarg18:dev_msrp_asrp_migration\n" +
+                    "+976 −15 \n" +
+                    " Conversation 20  Commits 2  Checks 0  Files changed 10\n" +
+                    " Open\n" +
+                    "CQ-4279355 export code for migration from MSRP to ASRP. #25\n" +
+                    "File filter... \n" +
+                    "0 / 8 files viewed\n" +
+                    " 137  ...ation/src/main/java/com/adobe/communities/ugc/migration/export/ActivityExportServlet.java \n" +
+                    "Viewed\n" +
+                    "@@ -0,0 +1,137 @@\n" +
+                    "Multi-line comments are here!\n" +
+                    "\n" +
+                    "You can now comment on multiple lines. Just click and drag on the  button.\n" +
+                    "\n" +
+                    "Demonstrating selecting multiple lines for commenting\n" +
+                    "\n" +
+                    "package com.adobe.communities.ugc.migration.export;\n" +
+                    "\n" +
+                    "import com.adobe.communities.ugc.migration.util.Constants;\n" +
+                    "import com.adobe.cq.social.activitystreams.api.SocialActivityManager;\n" +
+                    "import com.adobe.cq.social.activitystreams.api.SocialActivityStream;\n" +
+                    "import com.adobe.cq.social.scf.ClientUtilityFactory; parameters us not present. Exiting");
         }
 
 
@@ -66,25 +107,68 @@ public class SocialGraphExportServlet extends SlingSafeMethodsServlet {
             throw new ServletException("Cannot locate a valid resource at " + path);
         }
         final ValueMap vm = userRoot.adaptTo(ValueMap.class);
-        if (!vm.get("jcr:primaryType").equals("rep:AuthorizableFolder")) {
+        if (!vm.get(JCR_PRIMARY_TYPE).equals(AUTHORIZABLE_USER)) {
             throw new ServletException("Cannot locate a valid resource at " + path);
         }
-        //iterate over child resources to get user nodes
+
+        File outFile = null ;
         try {
-            writer.object();
-            exportSocialGraph(writer, userRoot, graph, relType, typeS);
-            writer.endObject();
-        } catch (final Exception e) {
-            throw new ServletException("Encountered a json exception while exporting social graph", e);
+            outFile = File.createTempFile(fileName, ".json");
+            if (!outFile.canWrite()) {
+                throw new ServletException("Cannot write to specified output file");
+            }
+            response.setContentType("application/octet-stream");
+            final String headerKey = "Content-Disposition";
+            final String headerValue = "attachment; filename=\""+fileName+".json\"";
+            response.setHeader(headerKey, headerValue);
+
+            FileOutputStream fos = new FileOutputStream(outFile);
+            BufferedOutputStream bos = new BufferedOutputStream(fos);
+            //loadMap(request) ;
+
+
+            OutputStream outStream = null;
+            InputStream inStream = null;
+            try {
+                responseWriter = new OutputStreamWriter(bos);
+                JSONWriter jsonWriter = new JSONWriter(responseWriter);
+                jsonWriter.setTidy(true);
+                jsonWriter.object() ;
+                exportSocialGraph(jsonWriter, userRoot, graph, relType, typeS);
+                jsonWriter.endObject();
+                responseWriter.flush();
+
+                IOUtils.closeQuietly(bos);
+                IOUtils.closeQuietly(fos);
+                // obtains response's output stream
+                outStream = response.getOutputStream();
+                inStream = new FileInputStream(outFile);
+                // copy from file to output
+                IOUtils.copy(inStream, outStream);
+            } catch (final IOException e) {
+                throw new ServletException(e);
+            } catch (final Exception e) {
+                throw new ServletException(e);
+            } finally {
+                IOUtils.closeQuietly(bos);
+                IOUtils.closeQuietly(fos);
+                IOUtils.closeQuietly(inStream);
+                IOUtils.closeQuietly(outStream);
+            }
+        } finally {
+            if (outFile != null) {
+                outFile.delete();
+            }
         }
     }
 
-    private void exportSocialGraph(final JSONWriter writer, final Resource resource, final SocialGraph graph, String relType, 
+    private void exportSocialGraph(
+            final JSONWriter writer, final Resource resource, final SocialGraph graph, String relType,
     		String typeS)
             throws JSONException, RepositoryException {
         Iterable<Resource> children = resource.getChildren();
         for (final Resource child : children) {
-            if (child.isResourceType("rep:User")) {
+            if (child.isResourceType(USER)) {
          
                 Vertex vertex = graph.getVertex(child.adaptTo(User.class).getID());
                 if(vertex == null) {
